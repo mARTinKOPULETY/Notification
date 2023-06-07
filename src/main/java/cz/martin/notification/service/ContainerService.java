75f9579dda00e20ea9cf4a48fb9f766cb2144d7f
mARTinKOPULETY <kopuletym@seznam.cz>
2023-06-07 10:50:25 +0200

    ContainerService.java - was replaced into containerService package.

--- src/main/java/cz/martin/notification/service/ContainerService.java
+++
@@ -1,8 +0,0 @@
-package cz.martin.notification.service;
-
-import cz.martin.notification.dto.ContainerDto;
-import cz.martin.notification.entity.Container;
-
-public interface ContainerService {
-    Container saveContainer(ContainerDto containerDto);
-}
